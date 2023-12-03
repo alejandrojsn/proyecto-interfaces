19495beb0279bfbecd025e8e199957d1adfd21a4
cesarisim0 <cedgo1997@gmail.com>
2023-12-03 18:56:55 -0400

    [FIX] Login: Fixing layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import { Link } from 'react-router-dom'
 
 function App() {
   return (
-    <>
       <div className='container'>
         <div className="row justify-content-center">
           <div className="col-6">
             <div className="vh-100 d-flex flex-column justify-content-evenly align-items-center">
-              <img src={IDGovLogo} alt="IDGov Logo" width="326px" height="202px"/>
+              <img src={IDGovLogo} alt="IDGov Logo" width="256px" />
               <button className='btn btn-outline-light btn-lg'>IDGov</button>
               <Link to="login">
                 <button className='btn btn-info btn-lg text-light text-nowrap'>Iniciar sesión</button>
@@ -20,7 +19,6 @@ function App() {
           </div>
         </div>
       </div>
-    </>
   )
 }
 
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,27 +1,30 @@
+import { Link } from 'react-router-dom'
 import IDGovLogo from './assets/IDGov.svg'
 
 export default function Login() {
   return (
-    <>
-      <div className='container'>
-        <div className="row justify-content-center">
-          <div className="col-6">
-            <div className="vh-100 d-flex flex-column justify-content-evenly align-items-center">
-              <img src={IDGovLogo} alt="IDGov Logo" width="326px" height="202px"/>
-              <form className="h-50 d-flex flex-column justify-content-evenly align-items-center">
-                <div className="w-75">
-                    <input type="email" className="form-control custom-input" name="email" placeholder="Correo" />
-                </div>
-                <div className="w-75">
-                    <input type="password" className="form-control custom-input" name="password" placeholder="Contraseña" />
-                </div>
-                <button type="submit" className="btn btn-secondary btn-lg w-75">Entrar</button>
-                </form>
-            </div>
+    <div className='container'>
+      <div className="row justify-content-center">
+        <div className="col-6">
+          <div className="vh-100 d-flex flex-column justify-content-center align-items-center">
+            <img src={IDGovLogo} alt="IDGov Logo" width="150px" />
+            <p className='login-title'>Iniciar sesión</p>
+            <form className="h-25 d-flex flex-column justify-content-evenly align-items-center">
+              <div className="w-100">
+                <input type="email" className="form-control custom-input" name="email" placeholder="Correo" />
+              </div>
+              <div className="w-100">
+                <input type="password" className="form-control custom-input" name="password" placeholder="Contraseña" />
+              </div>
+              <span className='login-register-link'>
+                ¿No tienes acceso? <Link to='/register'>Regístrate</Link>
+              </span>
+              <button type="submit" className="btn btn-secondary btn-md w-75 mt-3">Entrar</button>
+            </form>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
